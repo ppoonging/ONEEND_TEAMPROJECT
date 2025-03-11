16b2938b61b29e1ed431eb6623a85d11e7c51dec
myeongchanho <cksgh0726@naver.com>
2025-03-11 15:49:21 +0900

    날씨 구현 일단 서울에 국한하게 만들었고, 다른 지역은 우선 주석처리로 만들었음. 디자인 전부는 아니지만 색상은 어느정도 만져놨음

---
+++ src/main/java/com/springboot/biz/weather/WeatherResponse.java
@@ -0,0 +1,35 @@
+package com.springboot.biz.weather;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeatherResponse {
+        private String name; // 도시 이름
+        private Main main; // 날씨 정보
+        private Weather[] weather; // 날씨 상세
+
+        // Getters and Setters
+
+        @Getter
+        @Setter
+        public static class Main {
+                private double temp; // 기온
+                private double humidity; // 습도
+
+                // Getters and Setters
+        }
+
+        @Getter
+        @Setter
+        public static class Weather {
+                private String description; // 날씨 설명
+
+                // Getters and Setters
+        }
+}
+
+
+
