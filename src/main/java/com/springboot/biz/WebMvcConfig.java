ccb7e381182de904e5e5d23e9c762f6c3e7e0899
myeongchanho <cksgh0726@naver.com>
2025-03-16 14:47:32 +0900

    깃허브 수정중입니다. 화면이 안되서 일단 mvcconfig삭제했습니다. 지금 properties 이상하니까 참고,

--- src/main/java/com/springboot/biz/WebMvcConfig.java
+++ src/main/java/com/springboot/biz/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.springboot.biz;
 
+/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,12 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/files/mj/**")
-                .addResourceLocations("file:src/main/resources/static/files/mj/");
 
         registry.addResourceHandler("/**")
-                .addResourceLocations("file:src/main/resources/static/");
-
+                .addResourceLocations("classpath:/static/");
+    }
 
     }
-}
+*/
